e2aa540c90899047c48077f132a6a4476302037e
JS <jisup77@gmail.com>
2023-11-27 22:55:45 -0800

    14.2 bug fix

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -24,6 +24,7 @@ class _VideoPostState extends State<VideoPost>
   late final VideoPlayerController _videoPlayerController;
   final Duration _animationDuration = const Duration(milliseconds: 200);
   late final AnimationController _animationController;
+  late Future<void> _initializeVideoPlayerFuture;
   bool _isPaused = false;
 
   void _onVideoChange() {
@@ -35,16 +36,17 @@ class _VideoPostState extends State<VideoPost>
     }
   }
 
-  void _initVideoPlayer() async {
+  Future<bool> _initVideoPlayer() async {
     _videoPlayerController =
         VideoPlayerController.asset("assets/videos/video.mp4");
     await _videoPlayerController.initialize();
-    await _videoPlayerController.setLooping(true);
     if (kIsWeb == true) {
       await _videoPlayerController.setVolume(0);
     }
+    await _videoPlayerController.setLooping(true);
     _videoPlayerController.addListener(_onVideoChange);
-    setState(() {});
+    //setState(() {});
+    return true;
   }
 
   void _onTogglePause() {
@@ -75,7 +77,7 @@ class _VideoPostState extends State<VideoPost>
   @override
   void initState() {
     super.initState();
-    _initVideoPlayer();
+    _initializeVideoPlayerFuture = _initVideoPlayer();
 
     _animationController = AnimationController(
       vsync: this,
@@ -107,106 +109,113 @@ class _VideoPostState extends State<VideoPost>
 
   @override
   Widget build(BuildContext context) {
-    return VisibilityDetector(
-      key: Key("${widget.index}"),
-      onVisibilityChanged: _onVisibilityChanged,
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: _videoPlayerController.value.isInitialized
-                ? VideoPlayer(_videoPlayerController)
-                : Container(
-                    color: Colors.black,
-                  ),
-          ),
-          Positioned.fill(
-            child: GestureDetector(onTap: _onTogglePause),
-          ),
-          Positioned.fill(
-            child: IgnorePointer(
-              child: Center(
-                child: AnimatedBuilder(
-                  animation: _animationController,
-                  builder: (context, child) {
-                    return Transform.scale(
-                      scale: _animationController.value,
-                      child: child,
-                    );
-                  },
-                  child: AnimatedOpacity(
-                    opacity: _isPaused ? 1 : 0,
-                    duration: _animationDuration,
-                    child: const FaIcon(
-                      FontAwesomeIcons.play,
-                      color: Colors.white,
-                      size: Sizes.size52,
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return VisibilityDetector(
+            key: Key("${widget.index}"),
+            onVisibilityChanged: _onVisibilityChanged,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: VideoPlayer(_videoPlayerController),
+                ),
+                Positioned.fill(
+                  child: GestureDetector(onTap: _onTogglePause),
+                ),
+                Positioned.fill(
+                  child: IgnorePointer(
+                    child: Center(
+                      child: AnimatedBuilder(
+                        animation: _animationController,
+                        builder: (context, child) {
+                          return Transform.scale(
+                            scale: _animationController.value,
+                            child: child,
+                          );
+                        },
+                        child: AnimatedOpacity(
+                          opacity: _isPaused ? 1 : 0,
+                          duration: _animationDuration,
+                          child: const FaIcon(
+                            FontAwesomeIcons.play,
+                            color: Colors.white,
+                            size: Sizes.size52,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          ),
-          const Positioned(
-            bottom: 20,
-            left: 10,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "JS",
-                  style: TextStyle(
-                    fontSize: Sizes.size20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                const Positioned(
+                  bottom: 20,
+                  left: 10,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "JS",
+                        style: TextStyle(
+                          fontSize: Sizes.size20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Gaps.v10,
+                      Text(
+                        "This is my room in Bellevue!!!",
+                        style: TextStyle(
+                          fontSize: Sizes.size16,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Gaps.v10,
-                Text(
-                  "This is my room in Bellevue!!!",
-                  style: TextStyle(
-                    fontSize: Sizes.size16,
-                    color: Colors.white,
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Column(
+                    children: [
+                      const CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        foregroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/3612017"),
+                        child: Text("JS"),
+                      ),
+                      Gaps.v24,
+                      const VideoButton(
+                        icon: FontAwesomeIcons.solidHeart,
+                        text: "2.9M",
+                      ),
+                      Gaps.v24,
+                      GestureDetector(
+                        onTap: () => _onCommentsTap(context),
+                        child: const VideoButton(
+                          icon: FontAwesomeIcons.solidComment,
+                          text: "33K",
+                        ),
+                      ),
+                      Gaps.v24,
+                      const VideoButton(
+                        icon: FontAwesomeIcons.share,
+                        text: "Share",
+                      ),
+                    ],
                   ),
-                )
+                ),
               ],
             ),
-          ),
-          Positioned(
-            bottom: 20,
-            right: 10,
-            child: Column(
-              children: [
-                const CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/3612017"),
-                  child: Text("JS"),
-                ),
-                Gaps.v24,
-                const VideoButton(
-                  icon: FontAwesomeIcons.solidHeart,
-                  text: "2.9M",
-                ),
-                Gaps.v24,
-                GestureDetector(
-                  onTap: () => _onCommentsTap(context),
-                  child: const VideoButton(
-                    icon: FontAwesomeIcons.solidComment,
-                    text: "33K",
-                  ),
-                ),
-                Gaps.v24,
-                const VideoButton(
-                  icon: FontAwesomeIcons.share,
-                  text: "Share",
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return Container(
+            color: Colors.black,
+          );
+        }
+      },
     );
   }
 }
