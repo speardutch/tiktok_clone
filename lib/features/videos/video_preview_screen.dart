cf36d2663402f3c12462efa7a5ff9e2fabff6d4c
JS <jisup77@gmail.com>
2023-12-07 20:44:29 -0800

    19.8 ImagePicker

--- lib/features/videos/video_preview_screen.dart
+++ lib/features/videos/video_preview_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
@@ -17,6 +19,7 @@ class VideoPreviewScreen extends StatefulWidget {
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
+  bool _saveVideo = false;
 
   Future<void> _initVideo() async {
     _videoPlayerController = VideoPlayerController.file(
@@ -27,6 +30,18 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
     await _videoPlayerController.setLooping(true);
     await _videoPlayerController.play();
 
+    setState(() {});
+  }
+
+  Future<void> _saveToGallery() async {
+    if (_saveVideo) return;
+
+    await GallerySaver.saveVideo(
+      widget.video.path,
+      albumName: "TikTok Clone!",
+    );
+
+    _saveVideo = true;
     setState(() {});
   }
 
@@ -42,6 +57,14 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text("Preview video"),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(_saveVideo
+                ? FontAwesomeIcons.check
+                : FontAwesomeIcons.download),
+          ),
+        ],
       ),
       body: _videoPlayerController.value.isInitialized
           ? VideoPlayer(_videoPlayerController)
