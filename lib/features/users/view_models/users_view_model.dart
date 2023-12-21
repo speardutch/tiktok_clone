8ef0588c1ec119a10e26fdbd0b17c317bc3eb91b
JS <jisup77@gmail.com>
2023-12-20 19:22:53 -0800

    25.3 createProfile

---
+++ lib/features/users/view_models/users_view_model.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/users/models/user_profile_model.dart';
+import 'package:tiktok_clone/features/users/repos/user_repo.dart';
+
+class UserViewModel extends AsyncNotifier<UserProfileModel> {
+  late final UserRepository _repository;
+
+  @override
+  FutureOr<UserProfileModel> build() {
+    _repository = ref.read(userRepo);
+    return UserProfileModel.empty();
+  }
+
+  Future<void> createProfile(UserCredential credential) async {
+    if (credential.user == null) {
+      throw Exception("Account not created");
+    }
+
+    state = const AsyncValue.loading();
+    final profile = UserProfileModel(
+        uid: credential.user!.uid,
+        email: credential.user!.email ?? "anon@anon.com",
+        name: credential.user!.displayName ?? "Anon",
+        bio: "undefined",
+        link: "undefined");
+    await _repository.createProfile(profile);
+    state = AsyncValue.data(profile);
+  }
+}
+
+final userProvider = AsyncNotifierProvider<UserViewModel, UserProfileModel>(
+  () => UserViewModel(),
+);
