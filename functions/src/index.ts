0dc441520bb91396d06ba7664521e5ca15a8a88c
JS <jisup77@gmail.com>
2023-12-24 21:10:56 -0800

    26.5 publicUrl

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,5 +6,33 @@ admin.initializeApp();
 export const onVideoCreated = functions.firestore
     .document("vidoes/{videoId}")
     .onCreate(async (snapshot, context) => {
-        await snapshot.ref.update({ hello: "hello functions"});
+        const spawn = require("child-process-promise").spawn;
+        const video = snapshot.data();
+        await spawn("ffmpeg", [
+            "-i",
+            video.fileUrl,
+            "-ss",
+            "00:00:01.000",
+            "-vframes",
+            "1",
+            "-vf",
+            "scale=150:-1",
+            `/tmp/${snapshot.id}.jpg`,
+        ]);
+        const storage = admin.storage();
+        const [file, _] = await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+            destination: `thumbnails/${snapshot.id}.jpg`,
+        });
+        await file.makePublic();
+        await snapshot.ref.update({ thumbnailUrl: file.publicUrl() });
+
+        const db = admin.firestore();
+        await db
+            .collection("users")
+            .doc(video.creatorUid)
+            .collection("videos")
+            .doc(snapshot.id).set({
+                thumbnailUrl: file.publicUrl(),
+                videoId: snapshot.id,            
+            });
     });
